92c81dd8e8638749b49a58af1eabf385105b1154
Stephen <stephen.a.nerby@gmail.com>
2023-07-19 10:58:44 -0400

    Defined Orders table and associations - closes #16

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,18 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Product = require('./models/Product')
+const Order = require('./models/Order')
 
-//associations could go here!
+//associations here!
+User.hasMany(Order)
+Order.belongsTo(User)
 
 module.exports = {
   db,
   models: {
     User,
+    Product,
+    Order
   },
 }
