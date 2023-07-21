4ad30157f319add2176e778da8eea6dad34f2426
EricGoldmeier <erichuntergoldmeier@gmail.com>
2023-07-21 14:13:05 -0400

    still working on this

--- client/features/products/ProductCard.js
+++ client/features/products/ProductCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Product = ({product})=> {
     return(
-        <div>
+        <div className="product-card">
             {/* <img src={product.imageUrl}/> */}
             <p>{product.name}</p>
             <p>{product.description}</p>
