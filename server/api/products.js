c423a0c6b74ef8d23f3484190baab3254a7b981f
APatel-AI <arpan.p.se@gmail.com>
2023-07-21 05:19:29 -0500

    added put & delete routes  to let users update product quantity and remove items from cart -closes #40

--- server/api/products.js
+++ server/api/products.js
@@ -12,7 +12,7 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-module.exports = router; 
+
 
 
 
@@ -26,3 +26,28 @@ router.get('/:id', async(req,res,next)=>{
         next(err)
     }
 })
+
+// update product quantity: PUT /api/product/:productId
+router.put('/:id', async (req,res,next) => {
+  try{
+    const product = await Product.findByPk(req.params.id)
+    res.send(await product.update(req.body))
+  }
+  catch(err){
+    next(err)
+  }
+})
+
+// Delete product from cart: Delete /api/product/:productId
+router.delete('/:id', async (req,res,next) => {
+  try{
+    const product = await Product.findByPk(req.params.id);
+    await product.destroy();
+    res.json({message: 'Product Removed!' });
+  }
+  catch(err){
+    next(err);
+  }
+})
+
+module.exports = router; 
