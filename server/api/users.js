d65ba1eb530e53e0bd3cb5c331531e8f06179ab3
APatel-AI <arpan.p.se@gmail.com>
2023-07-23 13:21:06 -0500

    updated put route for better backend readability & testing, added route to access indivitual user carts in backend

--- server/api/users.js
+++ server/api/users.js
@@ -75,3 +75,79 @@ router.put("/:userId/cart/:productId", async (req, res, next) => {
     next(err);
   }
 });
+
+
+// Add a product to the cart for the logged-in user
+// POST /api/users/:userId/cart
+router.post("/:userId/cart", async (req, res, next) => {
+  try {
+    const { userId } = req.params.id;
+    const { productId } = req.body;
+
+    // Check if the logged-in user id is a match
+    if (req.params.id !== userId) {
+      return res.status(403).json({ message: "unauthorized user!" });
+    }
+
+    // Find the user and their associated cart
+    const user = await User.findByPk(userId, {
+      include: { model: Cart },
+    });
+
+    // Find the product to be added to the cart
+    const product = await Product.findByPk(productId);
+
+    // Check if the product exists
+    if (!product) {
+      return res.status(404).json({ message: "Product not found!" });
+    }
+
+    // Check if the product is already in the cart
+    const existingCartItem = user.cart.find((item) => item.id === productId);
+
+    if (existingCartItem) {
+      // If the product is already in the cart, update the quantity
+      await existingCartItem.cartProducts.update({
+        quantity: existingCartItem.cartProducts.quantity + 1,
+      });
+    } else {
+      // If the product is not in the cart, add it with quantity 1
+      await Cart.addProduct(product, { through: { quantity: 1 } });
+    }
+
+    // Fetch the updated user with the cart
+    const updatedUser = await User.findByPk(userId, {
+      include: { model: Cart },
+    });
+
+    res.json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+
+//get indivitual users cart: GET api/users/:userId/cart
+router.get("/:userId/cart", async (req,res,next) => {
+  try{
+    const { userId } = req.params; 
+
+    const user = await User.findByPk (userId, {
+      attributes: ["id", "email"], 
+      include: {
+        model: Cart, 
+        include: { model: Product}, 
+      }, 
+    })
+
+    res.json(user.cart); 
+  } catch (err) {
+    next(err)
+  }
+
+}); 
+
+//put request for cart (maybe)
+
+
